14b60b0ecfddcbf551d4413658515e79bfa01553
LautaroLeall <lautaroleal4@gmail.com>
2026-03-23 11:30:48 -0300

    page /complementos (add classname and styles)

--- src/page/Complementos.jsx
+++ src/page/Complementos.jsx
@@ -3,7 +3,7 @@ import ContactCom from '../components/ContactCom';
 
 const ComplementosPage = () => {
     return (
-        <div className="pt-35 px-5 md:px-8 pb-20 max-w-7xl mx-auto flex flex-col gap-12">
+        <div className="pt-33 px-5 md:px-10 pb-20 mx-auto flex flex-col gap-12">
             <Complementos />
             <ContactCom />
         </div>
