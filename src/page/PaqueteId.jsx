ba384310792c01582c6fa403e1e64a3f5d15fc62
LautaroLeall <lautaroleal4@gmail.com>
2026-03-23 11:32:40 -0300

    create new page PaquetesId /paquete:id (call component DetallesPaq.jsx)

---
+++ src/page/PaqueteId.jsx
@@ -0,0 +1,11 @@
+import DetallesPaq from '../components/DetallesPaq';
+
+const PaqueteId = () => {
+    return (
+        <div className="pt-33 px-5 md:px-10 pb-20 mx-auto">
+            <DetallesPaq />
+        </div>
+    );
+};
+
+export default PaqueteId;
