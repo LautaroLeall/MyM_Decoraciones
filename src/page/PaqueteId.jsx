c0fd859ecf34261b0fdcc19b0c392ff0dfef823c
LautaroLeall <lautaroleal4@gmail.com>
2026-03-24 19:41:21 -0300

    page /paquete:id (call component DetallesPaq)

--- src/page/PaqueteId.jsx
+++ src/page/PaqueteId.jsx
@@ -2,7 +2,7 @@ import DetallesPaq from '../components/DetallesPaq';
 
 const PaqueteId = () => {
     return (
-        <div className="pt-33 px-5 md:px-10 pb-20 mx-auto">
+        <div className="pt-25 pb-10">
             <DetallesPaq />
         </div>
     );
