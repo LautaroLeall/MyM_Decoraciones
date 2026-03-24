60863f11a65722d74ea57bc4c801fb60f20be4ff
LautaroLeall <lautaroleal4@gmail.com>
2026-03-24 19:34:50 -0300

    page /contratacion (contratar and requisitos)

--- src/page/Contratacion.jsx
+++ src/page/Contratacion.jsx
@@ -3,7 +3,7 @@ import Requisitos from '../components/Requisitos';
 
 const ContratacionPage = () => {
     return (
-        <div className="pt-35 px-5 md:px-55 pb-20 mx-auto flex flex-col gap-12">
+        <div className="py-25">
             <Contratar />
             <Requisitos />
         </div>
