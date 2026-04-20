1a8cf36b8c91619f11fe6ecc07a7cb0f46a5388b
LautaroLeall <lautaroleal4@gmail.com>
2026-04-19 21:57:17 -0300

    page home / (call new component process conditions)

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -2,12 +2,14 @@ import Hero from '../components/Hero';
 import Info from '../components/Info';
 import Paquetes from '../components/Paquetes';
 import CtaFinal from '../components/CtaFinal';
+import ProcessConditions from '../components/ProcessConditions';
 
 const Home = () => {
     return (
         <main className="home-main">
             <Hero />
             <Info />
+            <ProcessConditions />
             <div className="home-paquetes-wrapper flex flex-col">
                 <Paquetes />
             </div>
